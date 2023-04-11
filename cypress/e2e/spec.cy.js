496af5fc73e9a66ebd67eedf0cd49e61514089d7
omer canpolat <114859630+omercanpolat@users.noreply.github.com>
2023-04-11 16:06:01 +0300

    .eslinttrc.json added

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,16 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
-    cy.get("dropdown");
+describe("navbar test", () => {
+  it("navbar", () => {
+    cy.pause();
+    cy.visit("http://localhost:3000/");
+    cy.get("[data-test='movieHeader']").contains("React Movie App");
+    cy.get("[data-test='switchMode']").should("be.visible").dblclick();
+    cy.get("[data-test='userAvatar']").should("be.visible").click();
+    cy.get("[data-test='registerBtn']").should("be.visible").click();
+    cy.go("back");
+    cy.get("[data-test='userAvatar']").should("be.visible").click();
+    cy.get("[data-test='loginBtn']").should("be.visible").click();
+    cy.go("back");
+    cy.get("[data-test='userAvatar']").should("be.visible").click();
+    cy.get("[data-test='logoutBtn']").should("be.visible").click();
   });
 });
